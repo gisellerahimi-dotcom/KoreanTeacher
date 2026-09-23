a2beef6888950caa9b2b86472fec435fc5166d69
gisellerahimi-dotcom <gisellerahimi@gmail.com>
2026-09-23 17:27:37 -0400

    configure a development proxy in vite.config.js so that frontend can forward /api.chat requests to Flask

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+/*
+a development proxy forwards request from frontend server to
+backend server
+
+When React calls fetch('/api/chat'), the request goes to Vite. 
+A proxy tells Vite to forward requests starting with /api to Flask
+*/
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server : {
+    proxy : {
+      '/api' : {
+        target: 'http://127.0.0.1:5000',
+      },
+    },
+  },
 })
