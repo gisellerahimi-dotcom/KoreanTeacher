0a4a1470e9921876c06c421e5d01b6b3283b422c
gisellerahimi-dotcom <gisellerahimi@gmail.com>
2026-09-23 17:55:01 -0400

    creating ask_ai function..takes user's message, sends it to openAI, returns the reply as text

--- backend/ai.py
+++ backend/ai.py
@@ -1,11 +1,17 @@
-    
+
 from flask import Flask, jsonify, request
 from openai import OpenAI
+from pathlib import Path
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-# chatbot backend -> user sends message that is sent to flask
-# flask gets response back from ai and sends it to app.jsx
+#load the .env file located next to ai.py
+load_dotenv(Path(__file__).parent / ".env")
+
+#Reads OPENAI_API_KEY from the environment
+client = OpenAI()
+
 @app.route("/api/chat", methods=["POST"])
 def chat():
     data = request.get_json()
@@ -14,6 +20,20 @@ def chat():
 
     response = ask_ai(message)
 
+    #sendomg tp react
     return jsonify({
         "response": response
     })
+ 
+#create ask_ai to take the user's message, send it to OpenAI, and return
+#the reply as text   
+def ask_ai(message):
+    result = client.response.create(
+        model = "gpt-5-mini",
+        instructions = (
+            "You are a knowledgeable Korean language instructor"
+            "Explain concepts and provide translations clearly"
+        ),
+        input = message,
+    )
+    return result.output_text
